5b8d5bf022c87351f5be060bade0daa5307520e9
Ton Nom <ton@email.com>
2025-04-14 19:29:39 +0100

    Ready for Vercel deploy22354

--- Config/db.js
+++ Config/db.js
@@ -1,17 +1,18 @@
 // Config/db.js
 const { Sequelize } = require('sequelize');
+const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
   }
 );
-const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
 
 
 module.exports = sequelize;
